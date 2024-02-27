f19ddb094b306b64662bae333718632aadb26ec1
akashmn <akash003nandan@gmail.com>
2024-02-28 01:38:12 +0530

    2nd largest no. in array 3

--- python/find-second-maximum-number-in-a-list.py
+++ python/find-second-maximum-number-in-a-list.py
@@ -37,8 +37,32 @@
 ########################################################################################
 # Solution 2
 
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = list(set(map(int, input().split())))
+#     arr.sort(reverse=True)
+#     print(arr[1])
+
+########################################################################################
+# Solution 3
+
 if __name__ == '__main__':
     n = int(input())
-    arr = list(set(map(int, input().split())))
-    arr.sort(reverse=True)
-    print(arr[1])
+    arr = map(int, input().split())
+    
+    ls = list(arr)
+    
+    smax,maxim, i = -101, -101, 0
+
+    while i < len(ls): 
+    
+        if(ls[i] > maxim):
+            smax = maxim 
+            maxim = ls[i] 
+            
+        elif (ls[i] < maxim and ls[i] > smax):
+            smax = ls[i]
+            
+        i+=1
+
+    print(smax)
