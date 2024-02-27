f40f4f3ac136fa45d2ee25206d4c8572bb34adb9
akashmn <akash003nandan@gmail.com>
2024-02-28 01:25:20 +0530

    2nd largest no. in array 1

---
+++ python/find-second-maximum-number-in-a-list.py
@@ -0,0 +1,35 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+
+# Input Format
+
+# The first line contains . The second line contains an array   of  integers each separated by a space.
+
+# Constraints
+
+# Output Format
+
+# Print the runner-up score.
+
+# Sample Input 0
+
+# 5
+# 2 3 6 6 5
+# Sample Output 0
+
+# 5
+
+
+#######################################################################################
+# Solution 1
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    ls  = list(arr)
+
+    ls.sort()
+    max_A  = ls[-1]
+    B      = [i for i in ls if i<max_A]
+    B.sort()
+    second = B[-1]
+    print(second)
